2a208e597fbb1ae0e49b92890a0b42fb693dfafd
Royaleon <abhikumarsingh177@gmail.com>
2024-01-13 11:39:57 +0530

    test

--- src/test.java
+++ src/test.java
@@ -1,6 +1,10 @@
 public class test{
     public static void main(String[] args){
         System.out.println("hello");
+        System.out.println("hello2");
 
+    }
+    public static void name(){
+         System.out.println("hello3");
     }
 }
